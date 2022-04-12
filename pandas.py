2fa953bcc3a0d8ec50de3e4bc87cedbc7264152f
mariagarciau <91331262+mariagarciau@users.noreply.github.com>
2022-04-12 12:59:28 +0200

    top10csv

--- pandas.py
+++ pandas.py
@@ -1,3 +1,7 @@
 import pandas as pd
-data = pd.read_csv("fase2/spotify_top50_2021.csv")
+
+data = pd.read_csv("fase2\spotify_top50_2021.csv")
+
 top10 = data.head(10)
+df10 = pd.DataFrame({"artist": top10["artist_name"], "cancion" : top10["track_name"]})
+df10.to_csv("fase2\top10.csv")
