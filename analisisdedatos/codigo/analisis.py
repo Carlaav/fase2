7b9414af6b1926192c885fc2dc59c389ef4b9ed4
Carlaav <carla2002.av@gmail.com>
2022-04-14 01:35:56 +0200

    a

--- analisisdedatos/codigo/analisis.py
+++ analisisdedatos/codigo/analisis.py
@@ -1,1 +1,27 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+
+def descripcion(data):
+    print(data.info())
+
+def unicos(data):
+    print("id")
+    print(len(data["id"]) == len(set(data["id"])))
+
+    print("track_name")
+    print(len(data["track_name"]) == len(set(data["track_name"])))
+
+    print("track_id")
+    print(len(data["track_id"]) == len(set(data["track_id"])))
+
+def estadistica_columna(data):
+    print(data.describe())
+
+def graficar(data):
+    for column in data:
+        sns.histplot(x=data[column])
+        plt.show()
+    # Aqui se habria añadido el codigo correspondiente si se hubiera necesitado borrar filas 
+    # o añadir informacion
